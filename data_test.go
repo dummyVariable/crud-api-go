209942a4fb5680b5f4201af0e07e292a508f0aef
dummyVariable <officialsanjivee@gmail.com>
2020-10-02 12:13:45 +0530

    added test for update method

--- data_test.go
+++ data_test.go
@@ -5,16 +5,18 @@ import "testing"
 func Test_data_create(t *testing.T) {
 	startDB()
 
-	tests := []string{
-		"test1",
-		"test2",
+	tests := []struct {
+		ID      int
+		message string
+	}{
+		{1, "test1"},
+		{2, "test2"},
 	}
 
-	for i, testcase := range tests {
-		createItem(testcase)
-		id := i + 1
-		if data[id] != testcase {
-			t.Errorf("expected data to be test1 got:%v", data[id])
+	for _, testcase := range tests {
+		createItem(testcase.message)
+		if data[testcase.ID] != testcase.message {
+			t.Errorf("expected data to be test1 got:%v", data[testcase.ID])
 		}
 	}
 
@@ -24,17 +26,39 @@ func Test_data_create(t *testing.T) {
 }
 
 func Test_for_read(t *testing.T) {
-	tests := []string{
-		"test1",
-		"test2",
-		"None",
+	tests := []struct {
+		ID      int
+		message string
+	}{
+		{1, "test1"},
+		{2, "test2"},
+		{3, "None"},
 	}
 
-	for i, testcase := range tests {
-		id := i + 1
-		got := readItem(id)
-		if got != testcase {
-			t.Errorf("Expected %v but got %v", testcase, got)
+	for _, testcase := range tests {
+		got := readItem(testcase.ID)
+		if got != testcase.message {
+			t.Errorf("Expected %v but got %v", testcase.message, got)
 		}
+	}
+}
+
+func Test_for_update(t *testing.T) {
+	tests := []struct {
+		ID      int
+		message string
+		status  string
+	}{
+		{1, "test1updated", "Updated"},
+		{22, "None", "None"},
+	}
+
+	for _, testcase := range tests {
+		got := updateItem(testcase.ID, testcase.message)
+
+		if got != testcase.status {
+			t.Errorf("cannot update the item at ID %v", testcase.ID)
+		}
+
 	}
 }
